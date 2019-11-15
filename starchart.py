2f0585e07d16e8e79a760a24a5d691f9b690b47b
Jörg Weingrill <jweingrill@aip.de>
2019-11-15 14:58:59 +0100

    added bodies (planets and moon) and zodiac signs (sigh)

--- starchart.py
+++ starchart.py
@@ -15,9 +15,9 @@ import numpy as np
 from matplotlib import pyplot as plt
 from astropy import units as u
 from astropy.time import Time
-from astropy.coordinates import SkyCoord, EarthLocation, AltAz
+from astropy.coordinates import SkyCoord, EarthLocation, AltAz, get_body
 from astropy.io.votable import parse_single_table
-
+from warnings import warn
 
 def scaleto(values, bounds, k=None, d=None):
     """
@@ -59,8 +59,11 @@ class StarChart(object):
 
     def _coordinatetransform(self):
         self.Potsdam = EarthLocation(lat=52.4 * u.deg, lon=13.10 * u.deg, height=36 * u.m)
+        self.Beelitz = EarthLocation(lat=52.239976 * u.deg, lon=12.964697 * u.deg)
+        #TODO fix stupid summertime
         utcoffset = 1 * u.hour  # Eastern Daylight Time
-        self.time = Time('2019-12-01 23:00:00') - utcoffset
+        self.time = Time('2019-12-01 00:00:00') - utcoffset
+        self.local_time = self.time + utcoffset
         altaz = self.stars.transform_to(AltAz(obstime=self.time, location=self.Potsdam))
         i = np.where(altaz.alt.deg > 0.0)
         self.az = altaz.az[i].deg
@@ -70,52 +73,85 @@ class StarChart(object):
         self.theta = np.deg2rad(self.az)
         self.r = 90.0 - self.alt
 
+    def _bodies(self):
+        bodies = {'moon': '☽',
+                  'mercury': '☿',
+                  'venus': '♀',
+                  'mars': '♂',
+                  'jupiter': '♃',
+                  'saturn': '♄'}
+        for body_name in bodies:
+            body = get_body(body_name, self.time, location=self.Potsdam)
+
+            body_altaz = body.transform_to(AltAz(obstime=self.time, location=self.Potsdam))
+            theta = body_altaz.az.rad
+            r = 90.0 - body_altaz.alt.deg
+            if body_altaz.alt> 0.0:
+                plt.text(theta, r, bodies[body_name], color='darkred',
+                         horizontalalignment='center', verticalalignment='center',
+                         fontsize=8)
+
+    def _findstar(self, starname):
+        for i, name in enumerate(self.names):
+            if starname in name:
+                print(i, name)
+                return i
+        print('Cannot find %s in list' % starname)
+        return -1
+
     def _annotate(self, axis):
         names = [(b'3Alp Lyr', "Vega"),
                  (b'9Alp CMa', "Sirius"),
                  (b'13Alp Aur', "Capella"),
-                 (b'10Alp Cmi', "Procyon"),
+                 (b'10Alp CMi', "Prokyon"),
                  (b'58Alp Ori', "Betageuze"),
                  (b'Alp Tau', "Aldebaran"),
-                 (b'19Bet Ori', "Rigel")]
+                 (b'19Bet Ori', "Rigel"),
+                 (b'16Alp Boo', "Arktur"),
+                 (b'53Alp Aql', "Altair")]
         for name in names:
-            i = np.where(self.names == name[0])[0]
-            if i.size > 0:
+            try:
+                i = list(self.names).index(name[0])
+            except ValueError:
+                i = self._findstar(name[0])
+            if i >= 0:
                 axis.annotate(" "+name[1], (self.theta[i], self.r[i]), size=6, color='blue')
 
-    def _constellation(self, axis):
-        def findstar(starname):
-            for i, name in enumerate(self.names):
-                if starname in name:
-                    print(i, name)
-                    return i
-            raise ValueError(starname)
+    def zodiac(self):
+        zodiacs = {"Leo": ['10 41 11', '17 29 52', '♌'],
+                   "Can": ['08 40 22.20', '19 40 19.00', '♋'],
+                   "Vir": ['13 09 47.14', '-2 04 10.80', '♍'],
+                   "Gem": ['06 56 57.92', '22 57 54.14', '♊'],
+                   "Tau": ['04 47 11.90', '20 55 25.00', '♉'],
+                   "Ari": ['02 34 00.00', '20 58 36.00', '♈'],
+                   "Pis": ['00 37 39.80', '10 21 28.00', '♓']}
+        for key in zodiacs:
+            ra = zodiacs[key][0]
+            de = zodiacs[key][1]
+            sign = zodiacs[key][2]
+            coord = SkyCoord(ra=ra, dec=de, unit=(u.hourangle, u.deg)).transform_to(AltAz(obstime=self.time, location=self.Potsdam))
+            if coord.alt.deg > 0.0:
+                plt.text(coord.az.rad, 90.0 - coord.alt.deg, sign, color='darkgreen',
+                     horizontalalignment='center', verticalalignment='center',
+                     fontsize=6)
 
-        constellations = ([b'10Alp CMi', b'3Bet CMi'],
-                          [b'2Alp Tri', b'4Bet Tri', b'9Gam Tri', b'2Alp Tri'],
-                          [b'45Eps Cas', b'37Del Cas', b'27Gam Cas', b'18Alp Cas', b'11Bet Cas'],
-                          [b'21Alp And', b'53Bet Peg', b'54Alp Peg', b'88Gam Peg', b'21Alp And'],
-                          [b'50Alp Cyg', b'37Gam Cyg', b'21Eta Cyg', b'6Bet1Cyg'],
-                          [b'18Del Cyg', b'37Gam Cyg', b'53Eps Cyg'],
-                          [b'50Alp UMa', b'48Bet UMa', b'64Gam UMa', b'69Del UMa', b'50Alp UMa'],
-                          [b'69Del UMa', b'77Eps UMa', b'79Zet1UMa', b'85Eta UMa'],
-                          [b'53Kap Ori', b'50Zet1Ori', b'58Alp Ori', b'37Phi1Ori', b'24Gam Ori', b'34Del1Ori', b'19Bet Ori'],
-                          [b'112Bet Tau', b'37The Aur', b'34Bet Aur', b'13Alp Aur', b'10Eta Aur', b'3Iot Aur', b'112Bet Tau']
-                          )
+    def _constellation(self, axis):
+        from constellations import constellations
 
         for constellation in constellations:
             thetas = []
             rs = []
             names=[]
+
             while len(constellation) >= 1:
-                i0 = np.where(self.names == constellation[0])[0]
-                if len(i0) == 0:
-                    i0 = findstar(constellation[0])
-                if len(i0) > 1:
-                    raise ValueError(constellation[0])
-                thetas.append(self.theta[i0])
-                rs.append(self.r[i0])
-                names.append(self.names[i0])
+                try:
+                    i0 = list(self.names).index(constellation[0])
+                except ValueError:
+                    i0 = self._findstar(constellation[0])
+                if i0 >= 0:
+                    thetas.append(self.theta[i0])
+                    rs.append(self.r[i0])
+                    names.append(self.names[i0])
                 constellation = constellation[1:]
             try:
                 axis.plot(thetas, rs, color='darkblue', linewidth=0.5)
@@ -130,16 +166,20 @@ class StarChart(object):
         ax = fig.add_subplot(111, projection='polar')
 #        ax.plot(self.theta[k], self.r[k], ',', color='lightgray', markersize=0.5)
         ax.scatter(self.theta[i], self.r[i], s=area, color='k', )  # cmap='coolwarm_r'
+        self._bodies()
+
         ax.set_theta_zero_location('N')
         ax.set_theta_direction(1)
-        ax.set_thetagrids((0.0, 90.0, 180.0, 270.0), labels=('N', 'E', 'S', 'W'))
+        ax.set_thetagrids((0.0, 90.0, 180.0, 270.0), labels=('N', 'O', 'S', 'W'))
         self._constellation(ax)
+        self.zodiac()
         ax.set_rmax(90.0)
         ax.set_rmin(0.0)
         ax.set_rgrids((10.0, 30.0, 50.0, 70.0), labels='')
         self._annotate(ax)
 
-        plt.text(1.05, -0.07, 'Sternkarte für Potsdam %s, 2019 © Dr. Jörg Weingrill für die Sternfreunde Beelitz e.V.' % self.time,
+        plt.text(1.05, -0.07, 'Sternkarte für Potsdam %s Uhr, 2019 © Dr. Jörg Weingrill für die Sternfreunde Beelitz e.V.' %
+                 self.local_time.strftime("%d.%m.%Y %H:%M"),
                  horizontalalignment='right', verticalalignment='bottom',
                  fontsize=6, transform=ax.transAxes)
 
