87250e4456c785158dc7b74c0ae133af380f45f6
Jörg Weingrill <jweingrill@aip.de>
2019-11-15 14:57:58 +0100

    extracted constellation lines to separate file

---
+++ constellations.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = "Joerg Weingrill"
+__copyright__ = "Copyright 2019 Jörg Weingrill"
+__credits__ = ["Joerg Weingrill"]
+__license__ = "GPL"
+__version__ = "0.0.1"
+__maintainer__ = "Joerg Weingrill"
+__email__ = "jweingrill@gmail.com"
+__status__ = "Development"
+__date__ = "11/12/19"
+
+constellations = \
+    ([b'10Alp CMi', b'3Bet CMi'],
+    [b'2Alp Tri', b'4Bet Tri', b'9Gam Tri', b'2Alp Tri'],
+    [b'45Eps Cas', b'37Del Cas', b'27Gam Cas', b'18Alp Cas', b'11Bet Cas'],
+    [b'21Alp And', b'53Bet Peg', b'54Alp Peg', b'88Gam Peg', b'21Alp And'],
+    [b'50Alp Cyg', b'37Gam Cyg', b'21Eta Cyg', b'6Bet1Cyg'],
+    [b'18Del Cyg', b'37Gam Cyg', b'53Eps Cyg'],
+    [b'50Alp UMa', b'48Bet UMa', b'64Gam UMa', b'69Del UMa', b'50Alp UMa'],
+    [b'69Del UMa', b'77Eps UMa', b'79Zet1UMa', b'85Eta UMa'],
+    [b'53Kap Ori', b'50Zet1Ori', b'58Alp Ori', b'37Phi1Ori', b'24Gam Ori', b'34Del1Ori', b'19Bet Ori'],
+    [b'112Bet Tau', b'37The Aur', b'34Bet Aur', b'13Alp Aur', b'10Eta Aur', b'3Iot Aur', b'112Bet Tau'],
+    [b'3Alp Lyr', b'7Zet2Lyr', b'10Bet Lyr', b'14Gam Lyr', b'12Del2Lyr', b'7Zet2Lyr'],
+    [b'1Alp UMi', b'23Del UMi', b'22Eps UMi', b'16Zet UMi', b'7Bet UMi', b'13Gam UMi', b'21Eta UMi',
+     b'16Zet UMi'],
+    [b'24Gam Gem', b'43Zet Gem', b'55Del Gem', b'78Bet Gem', b'66Alp Gem', b'27Eps Gem', b'13Mu  Gem'],
+    [b'30Zet Boo', b'16Alp Boo', b'36Eps Boo', b'49Del Boo', b'42Bet Boo', b'27Gam Boo', b'25Rho Boo',
+     b'16Alp Boo', b'8Eta Boo'],
+    [b'58Eps Her', b'67Pi  Her', b'44Eta Her', b'40Zet Her', b'58Eps Her'],
+    [b'27Bet Her', b'40Zet Her'], [b'65Del Her', b'58Eps Her'], [b'85Iot Her', b'67Pi  Her'],
+    [b'22Tau Her', b'44Eta Her'],
+    [b'4The CrB', b'3Bet CrB', b'5Alp CrB', b'8Gam CrB', b'10Del CrB', b'13Eps CrB', b'14Iot CrB'],
+    [b'5Alp Cep', b'21Zet Cep', b'32Iot Cep', b'35Gam Cep', b'8Bet Cep', b'5Alp Cep', ],
+    [b'17Eps Leo', b'Mu  Leo', b'Zet Leo', b'Gam2Leo', b'Eta Leo', b'Alp Leo', b'70The Leo', b'Bet Leo',
+    b'Del Leo', b'70The Leo', ]
+    )
